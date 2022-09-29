79b336458b9f57aa166b248e78ff8cec80f0f001
nikbarsa <99321419+nikbarsa@users.noreply.github.com>
2022-09-29 22:35:50 +0300

    add css

--- app.js
+++ app.js
@@ -2,21 +2,60 @@ import React from 'react';
 import './App. css';
 
 function App() {
-    return (
-        <div className="App">
-            <Myhead />
-        </div>
-    );
-}
+    const [messageList, setMessageList] = useState([])
+    const [messageBody, setMessageBody = useState({
+        text: '',
+        autor: '',
+    })
 
+    const ROBOT_MESSAGE = 'Здраствуй,я прочитал твое сообщение'
+
+    useEffect(() => {
+        if (messageList.length > 0 && messageList.slice(-1)[0] autor !== 'robot'){
+        setTimeout(() => {
+            setMessageList(pervstate => [...pervstate, { text: ROBOT_MESSAGE, autor: 'robot' }])
+        }, 1500)
+    }
+}, [messageList]
+
+return (
+    <div className="App">
+        {<Form Data={messageBody} setData={setMessageBody} setMessage={setMessageList}></Form>}
+        <div className="messageList">
+            {
+                messageList.map((e, i) => <Message text={e.tezt} author={e.author} key={i />}))
+        }
+        </div>}
+    </div>
+);  
+}
 export default App;
 
+const {text, autor} = Data
 
-
-const Myhead = (Props) => {
-    return (
-        <div style={{ color: red }}>
-            привет,мир
-        </div>
+const SubmitForm = (e) =>{
+    e.preventDefault()
+    if(text.length > 0){
+        setMessage(pervstate => [...pervstate{text,author}])
+    }
+    setData(
+        {
+            text: '',
+            autor: '',
+        }
     )
 }
+
+
+return(
+    <form onSubmit={SubmitForm}>
+        <input placeholder='Имя' value={text} onChange = {(e)=> 
+setData(pervstate => ({...pervstate,text: e.target.value}))
+    )}/>
+
+    <input placeholder='Текст' value={author} onChange = {(e)=>
+    setData(pervstate => ({...pervstate,author: e.taret.value})
+    )}/>
+    <button type='submit'>Отправить</button>
+    </form>
+
